5badf52b5eb422902c7a269c4caa61b0f2fbab0a
Damien <mail@damientimewell.com>
2018-02-21 16:35:27 +0100

    Switching to rspec

---
+++ spec/api-blueprint/dry_struct_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe Car do
+  it { should be_kind_of(Dry::Struct) }
+end
--- test/apiblueprint_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApiBlueprint::Test < ActiveSupport::TestCase
-  test "truth" do
-    binding.pry
-    assert_kind_of Module, ApiBlueprint
-  end
-end
